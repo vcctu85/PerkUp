7ec184f0b9b4eee6965e55d454ec9e7fb9e49f07
Duo Xu <xxxuduo@berkeley.edu>
2018-11-12 20:49:54 -0800

    add msg page

---
+++ app/src/main/java/com/example/macbook/perkupapp/ChatPage.java
@@ -0,0 +1,16 @@
+package com.example.macbook.perkupapp;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class ChatPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_page);
+
+    }
+}
--- app/src/main/java/com/example/macbook/perkupapp/EnvironmentChoose.java
+++ app/src/main/java/com/example/macbook/perkupapp/EnvironmentChoose.java
@@ -1,21 +1,17 @@
 package com.example.macbook.perkupapp;
 
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class HomePage extends AppCompatActivity {
-
+public class EnvironmentChoose extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.choose_environment);
 
-
-        TextView textview = (TextView) findViewById(R.id.appLogo);
-        // adjust this line to get the TextView you want to change
-
+        TextView textview = (TextView) findViewById(R.id.appLogo);// adjust this line to get the TextView you want to change
         Typeface typeface = Typeface.createFromAsset(getAssets(),"AllertaStencil-Regular.ttf"); // create a typeface from the raw ttf
         textview.setTypeface(typeface);
 
