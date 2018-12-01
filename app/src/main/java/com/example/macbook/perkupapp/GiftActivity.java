9f467190efae4245a1902e3b9df56708efd0fd08
Duo Xu <xxxuduo@berkeley.edu>
2018-12-01 01:55:06 -0800

    solve the location bug

--- app/src/main/java/com/example/macbook/perkupapp/GiftActivity.java
+++ app/src/main/java/com/example/macbook/perkupapp/GiftActivity.java
@@ -13,6 +13,7 @@ import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -30,6 +31,7 @@ import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.example.macbook.perkupapp.StartLoc;
 
 public class GiftActivity extends Activity {
     private GiftAdapter giftAdapter;
@@ -37,24 +39,70 @@ public class GiftActivity extends Activity {
     private FusedLocationProviderClient mFusedLocationClient;
     private static int REQUEST_CODE = 36;
     private Location mLocation;
+    private double longitude;
+    private double latitude;
+    StartLoc startLoc;
+    private final static int REQUEST_ACCESS_LOCATION = 1021;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_gifts);
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        this.startLoc = new StartLoc(this);
+        requestPermissionIfNotGranted();
 
     }
+
+
+    public void requestPermissionIfNotGranted(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.i("StartPresenter", "Requesting Permission...");
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_ACCESS_LOCATION);
+        } else {
+            startLoc.updateLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_ACCESS_LOCATION:
+                startLoc.updateLocation();
+                break;
+        }
+    }
+
+    public void setCurrentLocation(double latitude, double longitude){
+        this.longitude = longitude;
+        this.latitude = latitude;
+        System.out.println(String.valueOf(latitude) + String.valueOf(longitude));
+
+        ListView newsListView = findViewById(R.id.list_view);
+        Gift one = new Gift("Caffe Strada", "2300 College Ave, Berkeley, CA 94704", false);
+        Gift two = new Gift("Jacobs Hall", "", true);
+        List<Gift> gifts = new ArrayList<>();
+        gifts.add(one);
+        gifts.add(two);
+        checkLocation(one);
+        giftAdapter = new GiftAdapter(this, gifts);
+        newsListView.setAdapter(giftAdapter);
+    }
+
+
     private boolean checkLocation(Gift g) {
         Geocoder geocoder;
         List<Address> addresses = null;
         geocoder = new Geocoder(this, Locale.getDefault());
         try {
-            addresses = geocoder.getFromLocation(mLocation.getLatitude(), mLocation.getLongitude(), 1);
+//            addresses = geocoder.getFromLocation(mLocation.getLatitude(), mLocation.getLongitude(), 1);
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
         } catch (IOException io) {
 
         }
-
         String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
         String city = addresses.get(0).getLocality();
         String state = addresses.get(0).getAdminArea();
@@ -66,108 +114,109 @@ public class GiftActivity extends Activity {
             return false;
         }
     }
-
-    /**
-     * call getLastLocation
-     */
-    private void getLocation() {
-        try {
-            mFusedLocationClient.getLastLocation()
-                    .addOnCompleteListener(this, new OnCompleteListener<Location>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Location> task) {
-                            if (task.isSuccessful() && task.getResult() != null) {
-                                mLocation = task.getResult();
-
-                            } else {
-
-                            }
-                        }
-                    });
-        } catch (SecurityException e) {
-
-        }
-
-    }
-    @Override
-    public void onStart() {
-        super.onStart();
-        //check if we have permission to the user's location
-        int p = ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION);
-        if (p == PackageManager.PERMISSION_GRANTED) {
-            //if not, request
-            requestPermissions();
-        } else {
-            //if so, just get the location
-            getLocation();
-        }
-        ListView newsListView = findViewById(R.id.list_view);
-        Gift one = new Gift("Caffe Strada", "2300 College Ave, Berkeley, CA 94704", false);
-        Gift two = new Gift("Jacobs Hall", "", true);
-        List<Gift> gifts = new ArrayList<>();
-        gifts.add(one);
-        gifts.add(two);
-        //checkLocation(one);
-        giftAdapter = new GiftAdapter(this, gifts);
-        newsListView.setAdapter(giftAdapter);
-    }
-
-    /**
-     * Cited: https://stackoverflow.com/questions/39781507/fused-location-provider-only-working-on-some-phones
-     * @param code
-     * @param permissions
-     * @param results
-     */
-    @Override
-    public void onRequestPermissionsResult(int code, @NonNull String[] permissions,
-                                           @NonNull int[] results) {
-        if (code == REQUEST_CODE) {
-            if (results[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted.
-                getLocation();
-            } else {
-                // Permission denied.
-                startSnackbar(R.string.warning, R.string.settings, new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent intent = new Intent();
-                        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                        Uri uri = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null);
-                        intent.setData(uri);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intent);
-                    }
-                });
-            }
-        }
-    }
-
-    private void startSnackbar(final int firstid, final int secondid,
-                               View.OnClickListener listener) {
-        Snackbar.make(findViewById(android.R.id.content),
-                getString(firstid),
-                Snackbar.LENGTH_INDEFINITE)
-                .setAction(getString(secondid), listener).show();
-    }
-
-
-    private void requestLocPermissions() {
-        ActivityCompat.requestPermissions(GiftActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
-    }
-
-    private void requestPermissions() {
-        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-            requestLocPermissions();
-        } else {
-            startSnackbar(R.string.warning, android.R.string.ok, new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    requestLocPermissions();
-                }
-            });
-
-        }
-    }
+//
+//    /**
+//     * call getLastLocation
+//     */
+//    private void getLocation() {
+//        try {
+//            mFusedLocationClient.getLastLocation()
+//                    .addOnCompleteListener(this, new OnCompleteListener<Location>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<Location> task) {
+//                            if (task.isSuccessful() && task.getResult() != null) {
+//                                mLocation = task.getResult();
+//                                mLongitudeLabel = mLocation.getLongitude();
+//                                mLatitudeLabel = mLocation.getLatitude();
+//                            } else {
+//
+//                            }
+//                        }
+//                    });
+//        } catch (SecurityException e) {
+//
+//        }
+//
+//    }
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        //check if we have permission to the user's location
+//        int p = ActivityCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_FINE_LOCATION);
+//        if (p == PackageManager.PERMISSION_GRANTED) {
+//            //if not, request
+//            requestPermissions();
+//        } else {
+//            //if so, just get the location
+//            getLocation();
+//        }
+//        ListView newsListView = findViewById(R.id.list_view);
+//        Gift one = new Gift("Caffe Strada", "2300 College Ave, Berkeley, CA 94704", false);
+//        Gift two = new Gift("Jacobs Hall", "", true);
+//        List<Gift> gifts = new ArrayList<>();
+//        gifts.add(one);
+//        gifts.add(two);
+//        checkLocation(one);
+//        giftAdapter = new GiftAdapter(this, gifts);
+//        newsListView.setAdapter(giftAdapter);
+//    }
+//
+//    /**
+//     * Cited: https://stackoverflow.com/questions/39781507/fused-location-provider-only-working-on-some-phones
+//     * @param code
+//     * @param permissions
+//     * @param results
+//     */
+//    @Override
+//    public void onRequestPermissionsResult(int code, @NonNull String[] permissions,
+//                                           @NonNull int[] results) {
+//        if (code == REQUEST_CODE) {
+//            if (results[0] == PackageManager.PERMISSION_GRANTED) {
+//                // Permission granted.
+//                getLocation();
+//            } else {
+//                // Permission denied.
+//                startSnackbar(R.string.warning, R.string.settings, new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        Intent intent = new Intent();
+//                        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+//                        Uri uri = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null);
+//                        intent.setData(uri);
+//                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                        startActivity(intent);
+//                    }
+//                });
+//            }
+//        }
+//    }
+//
+//    private void startSnackbar(final int firstid, final int secondid,
+//                               View.OnClickListener listener) {
+//        Snackbar.make(findViewById(android.R.id.content),
+//                getString(firstid),
+//                Snackbar.LENGTH_INDEFINITE)
+//                .setAction(getString(secondid), listener).show();
+//    }
+//
+//
+//    private void requestLocPermissions() {
+//        ActivityCompat.requestPermissions(GiftActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+//    }
+//
+//    private void requestPermissions() {
+//        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+//            requestLocPermissions();
+//        } else {
+//            startSnackbar(R.string.warning, android.R.string.ok, new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    requestLocPermissions();
+//                }
+//            });
+//
+//        }
+//    }
 
 }
---
+++ app/src/main/java/com/example/macbook/perkupapp/StartLoc.java
@@ -0,0 +1,53 @@
+package com.example.macbook.perkupapp;
+
+
+import android.annotation.SuppressLint;
+import android.location.Location;
+import android.os.Looper;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+public class StartLoc {
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location currentLoc;
+    private double mLatitudeLabel;
+    private double mLongitudeLabel;
+    GiftActivity ga;
+    public StartLoc(GiftActivity ga) {
+        this.ga = ga;
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(ga);
+    }
+
+    @SuppressLint("MissingPermission")
+    public void updateLocation(){
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(50);
+
+        mFusedLocationClient.requestLocationUpdates(locationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                Location location = locationResult.getLastLocation();
+                if (location != null) {
+                    Log.i("StartPresenter", location.toString());
+                    mLongitudeLabel = location.getLongitude();
+                    mLatitudeLabel = location.getLatitude();
+                    displayUpdatedLocation(location.getLatitude(), location.getLongitude());
+                    mFusedLocationClient.removeLocationUpdates(this);
+                } else
+                    Log.e("StartPresenter", "Location == null!");
+            }
+        }, Looper.myLooper());
+    }
+
+    private void displayUpdatedLocation(double latitude, double longitude){
+        ga.setCurrentLocation(latitude, longitude); //fallback solution
+    }
+
+
+}
